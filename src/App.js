e2073370d90b9acf9cbc75f39c34a46780e4ab33
IsraPerez98 <isra1998isra@gmail.com>
2020-10-14 20:32:39 -0300

    URLs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import URLs from './componentes/urls';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url_soap: "127.0.0.1:5000",
+      url_rest: "127.0.0.1:3000",
+    }
+
+    this.cambiarURLSoap = this.cambiarURLSoap.bind(this);
+    this.cambiarURLRest = this.cambiarURLRest.bind(this);
+  }
+  
+  cambiarURLSoap(url) {
+    this.setState({
+      url_soap: url
+    })
+  }
+
+  cambiarURLRest(url) {
+    this.setState({
+      url_rest: url
+    })
+  }
+
+  render() {
+    return (
+        <div className="app">
+          <URLs
+            url_soap={this.state.url_soap}
+            url_rest={this.state.url_rest}
+
+            cambiarURLSoap={this.cambiarURLSoap}
+            cambiarURLRest={this.cambiarURLRest}
+          />
+        </div>
+      );
+  }
 }
 
 export default App;
